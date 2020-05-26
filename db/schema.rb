3888ce910fbc73ffec98e7069f7423e30303ab32
blobbyblobfish <aywang6@gmail.com>
2020-05-26 12:26:04 -0400

    Added API

---
+++ db/schema.rb
@@ -0,0 +1,67 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_05_26_151452) do
+
+  create_table "board_images", force: :cascade do |t|
+    t.integer "board_id", null: false
+    t.integer "image_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["board_id"], name: "index_board_images_on_board_id"
+    t.index ["image_id"], name: "index_board_images_on_image_id"
+  end
+
+  create_table "boards", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "description"
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_boards_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "image_categories", force: :cascade do |t|
+    t.integer "image_id", null: false
+    t.integer "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_image_categories_on_category_id"
+    t.index ["image_id"], name: "index_image_categories_on_image_id"
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string "img_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "name"
+    t.string "password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "board_images", "boards"
+  add_foreign_key "board_images", "images"
+  add_foreign_key "boards", "users"
+  add_foreign_key "image_categories", "categories"
+  add_foreign_key "image_categories", "images"
+end
