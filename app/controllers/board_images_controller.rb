993d54920ac2fa05bd26d9590df24f009251cf1c
coralfussman <coralfussman@gmail.com>
2020-05-28 10:54:58 -0400

    styling

--- app/controllers/board_images_controller.rb
+++ app/controllers/board_images_controller.rb
@@ -1,5 +1,21 @@
 class BoardImagesController < ApplicationController
     before_action :find_board_image, only: :destroy
+
+    def new
+        #@board_image = BoardImage.new
+        redirect_to image_path(@image.id)
+    end
+
+    def create
+        board_image = BoardImage.create(board_image_params)
+        if board_image.valid?
+           
+           redirect_to board_image_path(board)
+        else
+           flash[:board_image_error] = board_image.errors.full_messages
+           redirect_to board_path(board_image.id)
+        end 
+    end
         
     def destroy
         @board_image.destroy
@@ -7,6 +23,9 @@ class BoardImagesController < ApplicationController
     end
     
     private
+    def board_image_params
+        params.require(:board_image).permit(:board_id, :user_id)
+    end
 
     def find_board_image
         @board_image = BoardImage.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   resources :board_images
   resources :boards
   resources :users
-  get '/', to: 'home_pages#home', as: "home_page"
+  root to: 'home_pages#home', as: 'home'
+  #get '/', to: 'home_pages#home', as: "home_page"
   get '/gallery', to: 'sessions#new', as: 'login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
